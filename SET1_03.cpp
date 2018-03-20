0f4ed693ab29afbf9998524d3277f85d4d3cd067
VASANTHAROHINI.P <36394938+vasanthsanmati@users.noreply.github.com>
2018-03-20 19:49:51 +0530

    Create SET1_03.cpp

---
+++ SET1_03.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	char n;
+	cin>>n;
+	if(n=='a' || n=='e' || n=='i' || n=='o'|| n=='u')
+	{
+		cout<<"vowel";
+		
+	}
+	else
+	{
+		cout<<"consonant";
+	}
+}
