690aaa4603026f367f43c028d3d2d012f676e6c1
VASANTHAROHINI.P <36394938+vasanthsanmati@users.noreply.github.com>
2018-04-06 18:02:11 +0530

    Create SET3_04.cpp

---
+++ SET3_04.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i,n,a[100],temp,j;
+    cin>>n;
+    for(i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+       for(i=0;i<n;i++)
+    {
+        for(j=i+1;j<n;j++)
+        {
+            if(a[i]>a[j])
+            {
+                temp=a[i];
+                a[i]=a[j];
+                a[j]=temp;
+            }
+        }
+    }
+    for(j=0;j<n;j++)
+    {
+        cout<<a[j]<<" ";
+    }
+    return 0;
+}
