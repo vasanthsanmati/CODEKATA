c9d53e5ae2882c540bbc0dd0052e3c841478bb44
VASANTHAROHINI.P <36394938+vasanthsanmati@users.noreply.github.com>
2018-04-09 19:28:31 +0530

    Create SET4_08.cpp

---
+++ SET4_08.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include <stdio.h>
+using namespace std;
+int main() 
+{
+int a,b,c;
+cout<<"a and b before swapping"<<endl;
+cin>>a>>b;
+a=a^b;
+b=a^b;
+a=a^b;
+cout<<"a and b after swapping"<<endl;
+cout<<a<<" "<<b;
+return 0;
+}
