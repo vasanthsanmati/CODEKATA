510d0f7a064e27324e74c0abdf6ea3f2671ff623
VASANTHAROHINI.P <36394938+vasanthsanmati@users.noreply.github.com>
2018-04-12 19:18:18 +0530

    Create SET5_04.cpp

---
+++ SET5_04.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<stdio.h>
+using namespace std;
+int main()
+{
+int n;
+cin>>n;;
+if(n>=1 && n<=10)
+{
+    cout<<"the number is range "<<n;
+}
+else
+{
+    cout<<"The number not in range "<<n;
+}
+return 0;
+}
