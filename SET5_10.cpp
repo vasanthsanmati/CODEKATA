065d19fe71f1e089f4dec8780d3c5aa73fbe1556
VASANTHAROHINI.P <36394938+vasanthsanmati@users.noreply.github.com>
2018-04-14 11:17:36 +0530

    Create SET5_10.cpp

---
+++ SET5_10.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include <stdio.h>
+using namespace std;
+int main()
+{
+int n;
+cin>>n;
+if(n%2==0)
+{
+    cout<<"It is a power of 2";
+}
+else
+cout<<"It is not a power of 2";
+return 0;
+}
