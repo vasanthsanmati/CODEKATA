cf200da720bd36942ba65f62ce9567da8d0bb19a
VASANTHAROHINI.P <36394938+vasanthsanmati@users.noreply.github.com>
2018-04-14 11:42:02 +0530

    Create SET6_02.cpp

---
+++ SET6_02.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include <stdio.h>
+using namespace std;
+int main()
+{
+int n;
+cin>>n;
+switch(n)
+{
+    case 1:
+    cout<<"one";
+    break;
+    case 2:
+    cout<<"two";
+    break;
+    case 3:
+    cout<<"three";
+    break;
+    case 4:
+    cout<<"four";
+    break;
+    case 5:
+    cout<<"five";
+    break;
+    case 6:
+    cout<<"six";
+    break;
+    case 7:
+    cout<<"seven";
+    break;
+    case 8:
+    cout<<"eight";
+    break;
+    case 9:
+    cout<<"nine";
+    break;
+    case 0:
+    cout<<"zero";
+    break;
+    default:
+    cout<<"no such numbers";
+}
+return 0;
+}
