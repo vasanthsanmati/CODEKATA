168a41f41d22d9277de487f6bf45c9841071170a
VASANTHAROHINI.P <36394938+vasanthsanmati@users.noreply.github.com>
2018-04-17 19:14:01 +0530

    Create SET7_02.cpp

---
+++ SET7_02.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include <stdio.h>
+using namespace std;
+int main()
+{
+    char a[50];
+    int i;
+    cin>>a[i];
+   if(a[i]=='0'||'1')
+   {
+   i++;
+   cout<<"yes";
+   }
+    else
+    {
+    i++;
+    cout<<"no";
+    }
+    return 0;
+}
