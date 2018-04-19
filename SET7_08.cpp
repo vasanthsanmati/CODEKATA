d3dac36b9033461d2f12ba4df6e657c399e821e3
VASANTHAROHINI.P <36394938+vasanthsanmati@users.noreply.github.com>
2018-04-19 18:26:03 +0530

    Create SET7_08.cpp

---
+++ SET7_08.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include <stdio.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    if(n%7==0)
+    {
+        cout<<"yes";
+    }
+    else
+    {
+        cout<<"no";
+    }
+
+    return 0;
+}
