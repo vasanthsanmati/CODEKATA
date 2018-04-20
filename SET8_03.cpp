62ab5416df66aaf48a122b9ab3ccd96701a7732a
VASANTHAROHINI.P <36394938+vasanthsanmati@users.noreply.github.com>
2018-04-20 19:21:25 +0530

    Create SET8_03.cpp

---
+++ SET8_03.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include <stdio.h>
+using namespace std;
+int main()
+{
+   int n,l,r;
+   cin>>n;
+   cin>>l>>r;
+   if(n>l&&n<r)
+   {
+       cout<<"yes";
+   }
+   else
+   {
+       cout<<"no";
+   }
+	return 0;
+}
